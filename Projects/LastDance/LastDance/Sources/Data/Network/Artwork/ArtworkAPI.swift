4e937febb0ff604a3bae64d968d3772fbc9e6091
aushinyan <iumd6075@gmail.com>
2025-10-08 17:46:13 +0900

    setting: 도메인별로 네트워크 분리 및 dto 추가

---
+++ Projects/LastDance/LastDance/Sources/Data/Network/Artwork/ArtworkAPI.swift
@@ -0,0 +1,40 @@
+//
+//  ArtworkAPI.swift
+//  LastDance
+//
+//  Created by 신얀 on 10/8/25.
+//
+
+import Moya
+
+enum ArtworkAPI {
+    
+}
+
+extension ArtworkAPI: BaseTargetType {
+    var path: String {
+        switch self {
+            
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+            
+        }
+    }
+    
+    var queryParameters: [String : Any]? {
+        switch self {
+            
+        }
+    }
+    
+    var bodyParameters: Codable? {
+        switch self {
+            
+        }
+    }
+}
+
+
