6cffa0c5f6eee247424f37ec213da8dbce867243
yanni13 <iumd6075@gmail.com>
2025-10-18 00:45:07 +0900

    ✨ [feature] ArtworkAPI 작품 전체 조회 api 작성

--- Projects/LastDance/LastDance/Sources/Data/Network/Artwork/ArtworkAPI.swift
+++ Projects/LastDance/LastDance/Sources/Data/Network/Artwork/ArtworkAPI.swift
@@ -8,31 +8,42 @@
 import Moya
 
 enum ArtworkAPI {
-    
+    case getArtworks(artist_id: Int?, exhibition_id: Int?)
 }
 
 extension ArtworkAPI: BaseTargetType {
     var path: String {
         switch self {
-            
+        case .getArtworks(let artist_id, let exhibition_id):
+            return "\(APIVersion.v1API)/artworks"
         }
     }
     
     var method: Moya.Method {
         switch self {
-            
+        case .getArtworks:
+            return .get
         }
     }
     
     var queryParameters: [String : Any]? {
         switch self {
-            
+        case .getArtworks(let artist_id, let exhibition_id):
+            var params: [String: Any] = [:]
+            if let artist_id = artist_id {
+                params["artist_id"] = artist_id
+            }
+            if let exhibition_id = exhibition_id {
+                params["exhibition_id"] = exhibition_id
+            }
+            return params.isEmpty ? nil : params
         }
     }
     
     var bodyParameters: Codable? {
         switch self {
-            
+        case .getArtworks:
+            return nil
         }
     }
 }
---
+++ Projects/LastDance/LastDance/Sources/Data/Network/Artwork/ArtworkAPIService.swift
@@ -0,0 +1,20 @@
+//
+//  ArtworkAPIService.swift
+//  LastDance
+//
+//  Created by 아우신얀 on 10/18/25.
+//
+
+import Foundation
+import Moya
+import SwiftData
+
+// MARK: ArtworkAPIServiceProtocol
+protocol ArtworkAPIServiceProtocol {
+
+}
+
+// MARK: ArtworkAPIService
+final class ArtworkAPIService: ArtworkAPIServiceProtocol {
+    
+}
