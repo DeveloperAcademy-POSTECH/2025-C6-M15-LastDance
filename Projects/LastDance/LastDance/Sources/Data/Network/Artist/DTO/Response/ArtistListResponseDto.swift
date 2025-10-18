6187fc17a7d77868d11a370b3e6180afbe485d5f
pearhyunjin <pearhyunjin@gmail.com>
2025-10-18 16:34:36 +0900

    ✨ [feature] 작가 정보 Request, Response DTO 구현

---
+++ Projects/LastDance/LastDance/Sources/Data/Network/Artist/DTO/Response/ArtistListResponseDto.swift
@@ -0,0 +1,16 @@
+//
+//  ArtistListResponseDto.swift
+//  LastDance
+//
+//  Created by 배현진 on 10/18/25.
+//
+
+struct ArtistListItemDto: Codable {
+    let id: Int
+    let uuid: String
+    let name: String
+    let bio: String?
+    let email: String?
+    let created_at: String
+    let updated_at: String?
+}
