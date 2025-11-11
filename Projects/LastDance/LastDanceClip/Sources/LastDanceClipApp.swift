923052992d4ea7677fcdd7003bbf9c56adc7b8d8
pearhyunjin <pearhyunjin@gmail.com>
2025-11-12 05:28:19 +0900

    ✨ [feat] AppClip을 위한 라우팅 구현

---
+++ Projects/LastDance/LastDanceClip/Sources/LastDanceClipApp.swift
@@ -0,0 +1,60 @@
+//
+//  LastDanceClipApp.swift
+//  LastDanceClip
+//
+//  Created by 배현진 on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct LastDanceClipApp: App {
+    @StateObject private var router = ClipNavigationRouter()
+
+    var body: some Scene {
+        WindowGroup {
+            ClipRootView()
+                .environmentObject(router)
+                .onAppear {
+                    // TODO: - 임시 확인을 위한 도메인 주소 지정.
+                    if router.path.isEmpty {
+                        if let url = URL(string: "https://nimble-griffin-1a0a69.netlify.app/artworkId/25?exhibitionId=9") {
+                            handle(url: url)
+                        }
+                    }
+                }
+                .onOpenURL { url in
+                    handle(url: url)
+                }
+                .onAppear(perform: UIApplication.shared.addTapGestureRecognizer)
+        }
+    }
+
+    /// URL → 라우트로 변환
+    private func handle(url: URL) {
+        let comps = URLComponents(url: url, resolvingAgainstBaseURL: false)
+        let pathComponents = url.pathComponents
+
+        let artworkId: Int = {
+            if pathComponents.count >= 3 {
+                return Int(pathComponents[2]) ?? 0
+            } else {
+                return 0
+            }
+        }()
+
+        let exhibitionId: Int = {
+            comps?.queryItems?
+                .first(where: { $0.name == "exhibitionId" })?
+                .value
+                .flatMap(Int.init) ?? 0
+        }()
+        
+        let route: ClipRoute = .artworkDetail(
+            artworkId: artworkId,
+            exhibitionId: exhibitionId
+        )
+
+        router.path = [route]
+    }
+}
