4f54778e6c7c1c134667bebd3411e495c7d1c5bf
pearhyunjin <pearhyunjin@gmail.com>
2025-11-19 23:32:16 +0900

    👷 [build] v1.2.3 배포를 위한 변경 사항 적용

--- Projects/LastDance/LastDanceClip/Sources/AppClipURLCoordinator.swift
+++ Projects/LastDance/LastDanceClip/Sources/AppClipURLCoordinator.swift
@@ -29,7 +29,7 @@ class AppClipURLCoordinator {
 
         let exhibitionId: Int = {
             comps?.queryItems?
-                .first(where: { $0.name == "show" })?
+                .first(where: { $0.name == "p" })?
                 .value
                 .flatMap(Int.init) ?? 0
         }()
