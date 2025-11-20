e3d65f164ee4de46275325028b5d5891d06532f0
yanni13 <iumd6075@gmail.com>
2025-11-20 12:43:48 +0900

    🐛 [fix] UserDefaults 키 관리 수정

--- Projects/LastDance/LastDance/Sources/Common/Manager/DeviceTokenManager.swift
+++ Projects/LastDance/LastDance/Sources/Common/Manager/DeviceTokenManager.swift
@@ -16,19 +16,20 @@ final class DeviceTokenManager {
 
     /// 디바이스 토큰을 UserDefaults에 저장
     func saveDeviceToken(_ token: String) {
-        UserDefaults.standard.set(token, forKey: "deviceToken")
+        UserDefaults.standard.set(token, forKey: UserDefaultsKey.deviceToken.rawValue)
         Log.debug("디바이스 토큰 저장 완료: \(token)")
     }
 
     /// 디바이스 토큰이 있고 visitorId/artistId가 있으면 서버로 전송
     func registerDeviceTokenIfNeeded() {
-        if UserDefaults.standard.bool(forKey: "hasRegisteredDeviceToken") {
+        if UserDefaults.standard.bool(forKey: UserDefaultsKey.hasRegisteredDeviceToken.rawValue) {
             Log.debug("디바이스 토큰 이미 등록됨")
             return
         }
 
         // UserDefaults에서 저장된 디바이스 토큰 가져오기
-        guard let token = UserDefaults.standard.string(forKey: "deviceToken") else {
+        guard let token = UserDefaults.standard.string(forKey: UserDefaultsKey.deviceToken.rawValue)
+        else {
             Log.debug("디바이스 토큰 없음 -> 나중에 등록 예정")
             return
         }
@@ -56,7 +57,8 @@ final class DeviceTokenManager {
             switch result {
             case .success:
                 // 등록 성공 시 플래그 저장하여 중복 등록 방지
-                UserDefaults.standard.set(true, forKey: "hasRegisteredDeviceToken")
+                UserDefaults.standard.set(
+                    true, forKey: UserDefaultsKey.hasRegisteredDeviceToken.rawValue)
                 Log.debug("서버에 디바이스 토큰 등록 성공 (최초 1회)")
             case .failure(let error):
                 Log.error("서버에 디바이스 토큰 전송 실패: \(error.localizedDescription)")
