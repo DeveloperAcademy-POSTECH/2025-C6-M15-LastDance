ad32b7d4d183e23575e425c8d7cd010529a52e59
yanni13 <iumd6075@gmail.com>
2025-11-21 11:21:59 +0900

    🔥 [remove] 불필요한 코드 및 파일 삭제

--- Projects/LastDance/LastDance/Sources/Presentation/Reaction/Views/ArtworkReactionView.swift
+++ Projects/LastDance/LastDance/Sources/Presentation/Reaction/Views/ArtworkReactionView.swift
@@ -57,7 +57,6 @@ struct ArtworkReactionView: View {
         .toolbarBackground(.hidden, for: .navigationBar)
         .ignoresSafeArea(edges: .top)
         .onAppear {
-            // 뷰가 나타날 때마다 최신 반응 데이터를 가져옴
             viewModel.fetchReactions()
         }
     }
